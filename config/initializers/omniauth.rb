9ca482b367483e4ec7561066d8a8f0d905ac1df9
kennygeiler <kennethlgeiler@gmail.com>
2015-06-08 10:53:12 -0400

    add keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
+  provider :facebook, ENV['FACEBOOK_ID_CLUB'], ENV['FACEBOOK_SECRET_CLUB'],
   :scope => 'email,user_friends'
 end
