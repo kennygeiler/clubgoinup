e97550a86ba98c699a50bdf061fde2d72efce176
kennygeiler <kennethlgeiler@gmail.com>
2015-06-09 09:06:51 -0400

    can get friends sort of

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ActiveRecord::Base
       user.save
     end
   end
+
+  def graph_api()
+    @graph = Koala::Facebook::API.new('CAACEdEose0cBAL3K9ksW8MNYgINViSZAVOSuXw4t7skPxhbAKPG18DZAYC5uKz3QmCNA7PRSvHiZCIKfB0je0j5juGiDck0wVlkSrlEQUZAQPZCBVEwfZCGtRvcOmR45Kf77XjA98ZATocINuylrwrzTxjmXaqqpkiNa50wRVBNDMtlKcoCMpPoFzP388J5ziuFICwCund9dRKVLX0U69PI')
+    friends = @graph.get_connections("me", "friends")
+    print friends
+  end
 end
