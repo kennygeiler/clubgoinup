458c80765bd5b234ac7e1e6e1aa04046131f2fe9
kennygeiler <kennethlgeiler@gmail.com>
2015-06-09 21:40:19 -0400

    going and not going to a venue

--- app/controllers/going_controller.rb
+++ app/controllers/going_controller.rb
@@ -7,7 +7,11 @@ class GoingController < ApplicationController
   end
 
   def update
-
+    @venue = Venue.find(params[:venue_id])
+    @going_up = Going.where(venue_id: @venue, user_id: current_user.id)[0]
+    if @going_up.update_attributes(going_params)
+      redirect_to venue_path(@going_up.venue_id)
+    end
   end
 
   private
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -7,6 +7,7 @@ class VenuesController < ApplicationController
   def show
     @venue = Venue.find(params[:id])
     @going = Going.new()
+    @going_up = Going.where(venue_id: @venue, user_id: current_user.id)[0]
   end
 
 end
