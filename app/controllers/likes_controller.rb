3f217105a833493c7e2e1507a7556aa4ff14f9d9
kennygeiler <kennethlgeiler@gmail.com>
2015-06-11 10:08:36 -0400

    change routes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
 
   def create
-
+    @like = Like.create()
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     resource :home, only: [:show]
     resources :venues, only: [:index, :show] do
       resources :going, only: [:new, :create, :update]
+    end
+
+    resources :users, only: [:show] do
       resources :likes, only: [:create]
     end
 
