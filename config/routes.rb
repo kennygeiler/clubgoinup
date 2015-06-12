6c4ee3e9a9a61a2692f5d47c2356bb7d496b6ffa
kennygeiler <kennethlgeiler@gmail.com>
2015-06-12 16:29:48 -0400

    add reply to messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
-    get 'auth/failure', to: redirect('/')
-    get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
-    resources :sessions, only: [:create, :destroy]
-    resource :home, only: [:show]
-    resources :venues, only: [:index, :show] do
-      resources :going, only: [:new, :create, :update]
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+  resources :venues, only: [:index, :show] do
+    resources :going, only: [:new, :create, :update]
+  end
+
+  resources :users, only: [:index]
+
+  resources :quotes, only: [:new, :create, :index, :show, :delete]
+  resources :promotors, only: [:new, :create, :delete]
+
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :reply
     end
+  end
+  resources :messages, only: [:new, :create]
 
-    resources :users, only: [:index]
-
-    resources :quotes, only: [:new, :create, :index, :show, :delete]
-    resources :promotors, only: [:new, :create, :delete]
-
-    resources :conversations, only: [:index, :show, :destroy]
-    resources :messages, only: [:new, :create]
-
-    root to: "quotes#new"
+  root to: "quotes#new"
 
 end
