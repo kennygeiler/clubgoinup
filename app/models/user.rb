c70b3511eb88d02376909ff56ee62be676562d98
kennygeiler <kennethlgeiler@gmail.com>
2015-06-11 20:39:09 -0400

    basic quote index

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :likes, foreign_key: :liker_id
   has_many :received_likes, class_name: "Like", foreign_key: :likee_id
   has_many :promotors
+  has_many :quotes
 
 
   def self.from_omniauth(auth)
