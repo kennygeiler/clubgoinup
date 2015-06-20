17b1f3ec52428cd86f67a63eb987f6cc565a42fa
kennygeiler <kennethlgeiler@gmail.com>
2015-06-20 12:31:49 -0400

    flash

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,8 @@ class MessagesController < ApplicationController
   def create
     recipients = User.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
-    flash[:success] = "Message has been sent!"
+    flash[:notice] = "Account successfully created"
     redirect_to conversation_path(conversation)
   end
 end
+
