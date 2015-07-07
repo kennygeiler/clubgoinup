2e70c27468a8f8f243797fb1792cd0e3fdd98715
kennygeiler <kennethlgeiler@gmail.com>
2015-07-07 12:24:41 -0400

    message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
   def new
     @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
-    @message_quote = Quote.find(params[:quote_id])
+    #@message_quote = Quote.find(params[:quote_id])
   end
 
   def create
@@ -11,4 +11,3 @@ class MessagesController < ApplicationController
     redirect_to conversation_path(conversation)
   end
 end
-
